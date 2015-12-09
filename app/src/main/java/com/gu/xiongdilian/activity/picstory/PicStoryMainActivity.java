e6c66ec354af871fc391b9a384e1bfbfb392c583
NateRobinson <15951084423@163.com>
2015-12-09 19:23:35 +0800

    添加创建图片故事界面

--- app/src/main/java/com/gu/xiongdilian/activity/picstory/PicStoryMainActivity.java
+++ app/src/main/java/com/gu/xiongdilian/activity/picstory/PicStoryMainActivity.java
@@ -11,16 +11,22 @@ import com.gu.xiongdilian.adapter.picstory.PicStoryAdapter;
 import com.gu.xiongdilian.base.XDLBaseWithCheckLoginActivity;
 import com.gu.xiongdilian.pojo.PicStory;
 import com.gu.xiongdilian.view.XListView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.PauseOnScrollListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.InjectView;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
 
 /**
  * Created by Nate on 2015/12/9.图片故事主页面
  */
-public class PicStoryMainActivity extends XDLBaseWithCheckLoginActivity {
+public class PicStoryMainActivity extends XDLBaseWithCheckLoginActivity implements XListView.IXListViewListener {
+    private static final int STATE_REFRESH = 0;// 下拉刷新
+    private static final int STATE_MORE = 1;// 加载更多
     @InjectView(R.id.pic_story_main_list)
     XListView picStoryMainList;
     @InjectView(R.id.list_empty_ll)
@@ -93,9 +99,19 @@ public class PicStoryMainActivity extends XDLBaseWithCheckLoginActivity {
     protected void initViewsAndEvents() {
         setCustomToolbar(ToolbarType.WITHBACK, "图片故事");
         mAdapter = new PicStoryAdapter(this, R.layout.pic_story_main_activity_item_layout, picStories);
+        // 上拉加载更多
+        picStoryMainList.setPullLoadEnable(true);
+        // 下拉加载
+        picStoryMainList.setPullRefreshEnable(true);
+        //Listview 在滑动过程中，停止加载图片
+        picStoryMainList.setOnScrollListener(new PauseOnScrollListener(ImageLoader.getInstance(), true, true));
         picStoryMainList.setAdapter(mAdapter);
         picStoryMainList.setEmptyView(listEmptyLl);
+        // 初次会请求第一页
+        queryData(STATE_REFRESH);
+        picStoryMainList.setXListViewListener(this);
     }
+
 
     /**
      * 网络连接连起来了
@@ -129,4 +145,64 @@ public class PicStoryMainActivity extends XDLBaseWithCheckLoginActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onRefresh() {
+        queryData(STATE_REFRESH);
+    }
+
+    @Override
+    public void onLoadMore() {
+        queryData(STATE_MORE);
+    }
+
+
+    /**
+     * 分页获取数据
+     */
+    private void queryData(final int actionType) {
+        BmobQuery<PicStory> query = new BmobQuery<>();
+        query.setLimit(pageSize); // 设置每页多少条数据
+        query.setSkip(currentPage * pageSize); // 从第几条数据开始，
+        query.include("author");// 希望在查询帖子信息的同时也把发布人的信息查询出来
+        query.order("-createdAt");// 按照更新时间降序排列
+        query.findObjects(this, new FindListener<PicStory>() {
+            @Override
+            public void onSuccess(List<PicStory> arg0) {
+                if (arg0.size() > 0) {
+                    picStoryMainList.stopRefresh();
+                    picStoryMainList.stopLoadMore();
+                    if (actionType == STATE_REFRESH) {
+                        // 当是下拉刷新操作时，将当前页的编号重置为0，并把bankCards清空，重新添加
+                        currentPage = 0;
+                        picStories.clear();
+                    }
+                    // 将本次查询的数据添加到bankCards中
+                    for (PicStory picStory : arg0) {
+                        picStories.add(picStory);
+                    }
+                    mAdapter.notifyDataSetChanged();
+                    // 这里在每次加载完数据后，将当前页码+1，这样在上拉刷新的onPullUpToRefresh方法中就不需要操作curPage了
+                    currentPage++;
+                } else {
+                    if (actionType == STATE_MORE) {
+                        picStoryMainList.stopLoadMore();
+                        picStoryMainList.stopRefresh();
+                        showToast("没有更多数据了");
+                    } else if (actionType == STATE_REFRESH) {
+                        picStoryMainList.stopLoadMore();
+                        picStoryMainList.stopRefresh();
+                        showToast("没有数据");
+                    }
+                }
+
+            }
+
+            @Override
+            public void onError(int arg0, String arg1) {
+                showToast("哎呀,查询失败了");
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/gu/xiongdilian/adapter/picstory/PicStoryAdapter.java
+++ app/src/main/java/com/gu/xiongdilian/adapter/picstory/PicStoryAdapter.java
@@ -1,9 +1,12 @@
 package com.gu.xiongdilian.adapter.picstory;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.gu.baselibrary.baseadapter.BaseViewHolder;
 import com.gu.baselibrary.baseadapter.MyBaseAdapter;
+import com.gu.baselibrary.utils.DrawableUtils;
+import com.gu.xiongdilian.R;
 import com.gu.xiongdilian.pojo.PicStory;
 
 import java.util.List;
@@ -25,6 +28,10 @@ public class PicStoryAdapter extends MyBaseAdapter<PicStory> {
      */
     @Override
     public void setConvert(BaseViewHolder viewHolder, PicStory picStory) {
-
+//        if (TextUtils.isEmpty(picStory.getAuthor().getAvatar())) {
+//            DrawableUtils.disPlayLocRoundCornerImg(headImg, R.mipmap.default_xiongdilian_headimg);
+//        } else {
+//            DrawableUtils.displayRoundCornerImgOnNet(headImg, picStory.getAuthor().getAvatar());
+//        }
     }
 }
